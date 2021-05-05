4a3cb5a761bbc9b8819179a153b42929fb0eecde
Patrick <patrick01.programmer@gmail.com>
2021-05-05 08:05:46 +0000

    I have added 'isActive' column to 'BranchProduct' model.

--- src/sequelize/models/branchproduct.js
+++ src/sequelize/models/branchproduct.js
@@ -15,6 +15,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       defaultValue: 0,
     },
+    isActive: {
+      defaultValue: false,
+      type: DataTypes.BOOLEAN
+    },
     createdAt: {
       allowNull: false,
       type: DataTypes.BIGINT,
